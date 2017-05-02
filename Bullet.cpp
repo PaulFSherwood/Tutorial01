4feb25fdf2b9bfb6676b405922cd0095027a2116
paul <paulfsherwood@gmail.com>
2017-05-02 17:38:18 -0600

    added delete bullets

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,7 +1,9 @@
 #include "bullet.h"
+#include "enemy.h"
 #include <QTimer>
 #include <QGraphicsScene>
 #include <QDebug>
+#include <QList>
 
 Bullet::Bullet()
 {   // draw the rect
@@ -16,6 +18,19 @@ Bullet::Bullet()
 
 void Bullet::move()
 {
+    // if bullet collides with enemy, destroy both
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for (int i = 0, n = colliding_items.size(); i < n; ++i){
+        if (typeid(*(colliding_items[i])) == typeid(Enemy)) {
+            // remove them both
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+            // delete them both
+            delete colliding_items[i];
+            delete this;
+            return;
+        }
+    }
     // move bullet up
     setPos(x() , y()-10);
     if (pos().y()+rect().height() < 0){
